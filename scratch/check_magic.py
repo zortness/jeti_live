8c61ee893f665b380ff709bd420652d7f485e375
Kurtis <zortness@gmail.com>
2026-06-07 18:03:25 -0700

    removing scratch files

--- scratch/check_magic.py
+++
@@ -1,2 +0,0 @@
-with open("live_capture.pcap", "rb") as f:
-    print(f.read(32).hex())
--- scratch/check_pcap_magic.py
+++
@@ -1,3 +0,0 @@
-with open("live_capture.pcap", "rb") as f:
-    header = f.read(16)
-    print("Magic bytes:", header.hex(" ").upper())
